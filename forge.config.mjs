d4e10a84fb988eff64ecd1a48307d549c83862ae
rutan <ru_shalm@hazimu.com>
2024-11-16 00:32:46 +0900

    パッケージ時にアイコンを設定するように

--- forge.config.mjs
+++ forge.config.mjs
@@ -1,5 +1,7 @@
 import { join, relative } from "node:path";
+import { copyFile, mkdir, writeFile } from "node:fs/promises";
 import ignore from "ignore";
+import pngToIco from "png-to-ico";
 import { APP_DIR_PATH } from "./electron/constants.mjs";
 import { readSyncRpgMakerPluginConfig } from "./electron/modules/rpgMakerConfig.mjs";
 
@@ -26,15 +28,38 @@ packageIgnore.add([
   "/app/save",
 ]);
 
+const iconFilePath = join(APP_DIR_PATH, "icon/icon.ico");
+const pngFilePath = join(APP_DIR_PATH, "icon/icon.png");
+const tmpDirPath = join("tmp");
+const tmpIconDirPath = join(tmpDirPath, "icon");
+
 export default {
   packagerConfig: {
     name: packageName,
+    icon: join(tmpIconDirPath, "icon"),
     asar: useAsar && {
-      unpackDir: 'node_modules/steamworks.js/dist',
+      unpackDir: "node_modules/steamworks.js/dist",
     },
     ignore: (filePath) => {
       const relativePath = relative("/", filePath);
       return packageIgnore.ignores(relativePath);
     },
-  }
+  },
+  hooks: {
+    // パッケージング前にアイコンを用意するフック
+    prePackage: async () => {
+      await mkdir(tmpIconDirPath, { recursive: true });
+
+      const tmpIconFilePath = join(tmpIconDirPath, "icon.ico");
+
+      try {
+        await copyFile(iconFilePath, tmpIconFilePath);
+      } catch (_) {
+        // ICO ファイルが存在しない場合は PNG から ICO を生成
+        await pngToIco(pngFilePath).then((buffer) => {
+          return writeFile(tmpIconFilePath, buffer);
+        });
+      }
+    },
+  },
 };
